33d8f3cf91fc2c092561b8cfeb84e9ffd74a8730
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-03-05 14:56:24 -0500

    comments for clarity

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 var bandIs = function (){
     var bandQuery = $("#bandName").val()
-    //  the bandQuery variable will be updated to take in user input
+    //  Takes in user input
     var queryURL = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=bandit"
     //  band is in town api
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(resultsEvent){
-        // console.log(resultsEvent)
+        // looping through the array of upcoming events
         for ( var i =0; i<resultsEvent.length; i++){
             console.log(resultsEvent[i])
             var venue = resultsEvent[i].venue;
-            var div = $("<div>");
-            var city = $("<p>").text(venue.city);
             var lat = venue.latitude;
             var long = venue.longitude;
+            var div = $("<div>");
+            var city = $("<p>").text(venue.city);
             var name = $("<p>").text(venue.name).attr({
-                // setting data types to the venue so that 
+                "class": "venue", 
+                // Venue location information  is set to the the data types below
+                "data-venue": venue.name,
                 "data-city": venue.city,
-                "class": "venue",
                 "data-lat": lat,
                 "data-long": long
             });
-
             div.append(city, name);
             $("body").append(div);
         }
@@ -30,7 +30,7 @@ var bandIs = function (){
 }
 bandIs();
 
-// 
+// This is testing to make sure that the data types were
 $(document).on("click", ".venue", function(){
     var lati = $(this).attr("data-lat")
     var longi = $(this).attr("data-long")
