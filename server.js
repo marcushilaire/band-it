671f7934a24ada73f50baea6a62d1794f4448096
Woojoo Byun <woojoobyun@Woojoos-MacBook-Pro.local>
2018-03-06 10:22:48 -0500

    server is ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var yelp = require('yelp-fusion');
 var express= require("express");
 var bodyParser = require("body-parser")
+var path = require("path");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
@@ -10,7 +11,16 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
+app.get("/band/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./results.html"));
+  });
+  // If no matching route is found default to home
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
 // yelp search using node
 var apiKey = '-LJE5gdMKh7r6T5o7m7eg3q02j_lbmsiS0GEW57DcF0ucR9PdtP_CGJ1Ceq-JipoX9XyG9oThAb8snqi3bYmS0YLOVxUtHZAdlssf3tHikHmK1jTgJaw8nNjiw6TWnYx';
 var client = yelp.client(apiKey);
@@ -48,6 +58,9 @@ app.post("/yelp", function(req, res){
     console.log(searchedArr);
   });
 })
+
+
+
 
 
 
