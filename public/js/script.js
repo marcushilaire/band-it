846bd94d4ba2301d6d839742783a098337550a12
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-07 01:23:40 -0500

    added init map function, set variable to returned data values from yelp., added div to display yelp data.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+var markers = [];
 
 var bandIs = function (){
   var name = window.location.href;
@@ -15,7 +16,7 @@ var bandIs = function (){
 
         // looping through the array of upcoming events
         for ( var i =0; i<resultsEvent.length; i++){
-            console.log(resultsEvent[i])
+            // console.log(resultsEvent[i])
             var venue = resultsEvent[i].venue;
             var lat = venue.latitude;
             var long = venue.longitude;
@@ -47,14 +48,18 @@ var bandIs = function (){
   // function that occurs when the user pick a single venue
 
   $(".venue").on("click",function(){
-    console.log("YOOOOOOOOO")
+    // console.log("YOOOOOOOOO")
   });
+
 var yelpfunction=function(){
   $(".venue").on("click", function(){
-    console.log("click");
+    var newVenue = $(this).attr("data-venue")
+    console.log(newVenue);
     var lati = $(this).attr("data-lat")
     var longi = $(this).attr("data-long")
     var userSelects="bar, restaurant"; //have user select which catagory to select in html so they can choose what stores
+    console.log(lati);
+    console.log(longi);
 
     var newSearchRequest= {
       categories: userSelects,
@@ -63,7 +68,21 @@ var yelpfunction=function(){
     }
     $.post("/yelp", newSearchRequest, function(data){
       // console loging all the data as array and json object
-      console.log(data);
+      // console.log(data);
+
+      // loop to go log all data in the array
+      for (var i = 0; i < data.length; i++) {
+        var yelpResultsCard = $("<p>")
+        var yelpName = data[i].name
+        var add1 = data[i].address[0]
+        var add2 = data[i].address[1]
+        var add3 = data[i].address[2]
+        var yelpPhone = data[i].phone
+        var yelpPrice = data[i].price
+        var yelpRating = data[i].rating
+        $("#yelpResults").append(yelpName, add1, add2, add3, yelpPhone)
+      }
+
       // use this to get google map intergration and info we want to give out as output for all the store info
     });
   });
@@ -91,4 +110,87 @@ $("#submitBtn").on("click", function(event){
 //     console.log($(this).attr("data-city"))
 // })
 bandIs();
+
+//* Google Maps JS *//
+// get goole map function with marker set desired location
+function initMap() {
+
+  var theAnthem = {lat: 38.8848049, lng: -77.0302294} // replace capitalGrill with venue lati and longi
+  var map = new google.maps.Map(document.getElementById('artistMap'), {
+    zoom: 14, // zoom in to neighborboods near the venue
+    center: theAnthem
+  });
+  // var image = 'https://png.icons8.com/color/50/000000/music.png'
+  var marker = new google.maps.Marker({
+    position: theAnthem,
+    map: map
+    // icon: image,
+
+  })
+// content of clicking on the map marker.  Populate from yelp data later.
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 400
+
+  })
+// listen for the music marker click
+  marker.addListener('click', function() {
+    infowindow.open(map, marker)
+  })
+}
+// initMap()
+
+// Adds a marker to the map and push to the array.
+      function addMarker(location) {
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        markers.push(marker);
+      }
+
+// Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setMapOnAll(map);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+      }
+
+      // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
 //yelpfunction();
